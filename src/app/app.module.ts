e8703a2fbdf3afc4bdeff14df9abe080e74af1d4
allynpan <allynpan@yahoo.com>
2020-12-17 15:04:30 +0800

    对module做了注释工作

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent //配置当前项目运行的组件
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,  // 配置当前模块运行依赖的其他模块
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],  //配置项目所需要的服务，个人理解应该是Tomcat类型的东西
+  bootstrap: [AppComponent] // 指定应用的主视图，应该是开始的根组件，可以理解为开头第一次看到的网页界面这些
 })
 export class AppModule { }
